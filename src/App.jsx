d66ae8defa39a3d98d2c4d2c0f9d4c3a1731fcb0
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-06-26 21:35:12 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import { gsap } from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-black text-5xl font-bold underline'>
+        Mojito
+      </h1>
+    </div>
+  )
+}
+
+export default App
