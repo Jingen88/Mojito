8a331d7e5e00e1387cb9ddcbbe3d26240debe4af
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-06-27 22:46:50 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,51 +33,53 @@ useGSAP(() => {
  
 
   return (
-    <footer id='contact'>
-        <img src= 'images/footer-left-leaf.png' alt='left-leaf' id='f-left-leaf'/>
-        <img src= 'images/footer-right-leaf.png' alt='right-leaf' id='f-right-leaf'/>
-    
-    <div className='content'>
-        <h2>Where to Find Us</h2>
-
-        <div>
-            <h3>Visit Our Bar</h3>
-            <p>123 Mojito Lane, Cocktail City, CC 12345</p>
-            
-        </div>
-
-        <div>
-            <h3>Contact Us</h3>
-            <p>Phone: (123) 456-7890</p>
-            <p>hello@mocktail.com</p>
-        </div>
-
-        <div>
-            <h3>Opening Hours</h3>
-                {openingHours.map((time) => (
-                    <p key={time.day}>{time.day} : {time.time}</p>
-                ))}
-        </div>
-<div>
-<h3>Follow Us</h3>
-<div className='flex-center gap-5'>
-    
-    {socials.map((social) => (
-        <a key={social.name} 
-        href={social.url} 
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={social.name}>
-
-            <img src={social.icon}/>
-            </a> ))}
-</div>
-</div>
-    </div>
-    
-    
-    </footer>
-  )
+	<footer id="contact">
+	 <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
+	 <img src="/images/footer-left-leaf.png" alt="leaf-left" id="f-left-leaf" />
+	 
+	 <div className="content">
+		<h2>Where to Find Us</h2>
+		
+		<div>
+		 <h3>Visit Our Bar</h3>
+		 <p>4567, St.Patrick Blvd, Langley, CA</p>
+		</div>
+		
+		<div>
+		 <h3>Contact Us</h3>
+		 <p>(555) 987-65678</p>
+		 <p>hello@mocktail.com</p>
+		</div>
+		
+		<div>
+		 <h3>Open Every Day</h3>
+		 {openingHours.map((time) => (
+			<p key={time.day}>
+			 {time.day} : {time.time}
+			</p>
+		 ))}
+		</div>
+		
+		<div>
+		 <h3>Follow Us</h3>
+		 
+		 <div className="flex-center gap-5">
+			{socials.map((social) => (
+			 <a
+			 	key={social.name}
+				href={social.url}
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label={social.name}
+			 >
+				<img src={social.icon} />
+			 </a>
+			))}
+		 </div>
+		</div>
+	 </div>
+	</footer>
+ )
 }
 
 export default Contact
