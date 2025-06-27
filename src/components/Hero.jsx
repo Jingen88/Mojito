71612fd6c0af1f7f484b09b1e05ef1b1ab702b6e
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-06-27 12:11:21 +0100

    ok

---
+++ src/components/Hero.jsx
@@ -0,0 +1,124 @@
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import { gsap } from 'gsap'
+import React, { use } from 'react'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+
+const Hero = () => {
+
+const videoRef =useRef()
+const isMobile =useMediaQuery({ maxWidth: 767 })
+
+
+useGSAP(()=>{
+    const heroSplit = new SplitText('.title', {
+        type: 'words, chars',
+    });
+
+     const paragraphSplit = new SplitText('.subtitle', {
+        type: 'lines',
+    });
+
+heroSplit.chars.forEach((char) => char.classList.add ('text-gradient'))
+
+gsap.from(heroSplit.chars, {
+    yPercent: 100,
+    duration: 1.8,
+    ease: 'expo.out',
+    stagger: 0.06,
+})
+gsap.from(paragraphSplit.lines, {
+    opacity: 0,
+    yPercent: 100,
+    duration: 1.8,
+    ease: 'expo.out',
+    stagger: 0.06,
+    delay: 1,
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '#hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+    },
+})
+
+.to('.right-leaf',{y:200},0)
+.to('.left-leaf',{y:-200},0)
+
+const startValue = isMobile? 'top 50%': 'center 60%';
+const endValue = isMobile? '120% top' :'bottom top';
+
+let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
+	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	};
+
+},[]);
+
+
+
+
+  return (
+    <>
+    <section id='hero' className="noisy">
+<h1 className='title'>Mojito</h1>
+
+<img src='/images/hero-left-leaf.png'
+alt='left leaf' 
+className='left-leaf' />    
+
+  <img src='/images/hero-right-leaf.png'
+alt='right leaf' 
+className='right-leaf' />     
+    
+    <div className='body'>
+        <div className='content'>
+            <div className='space-y-5 hidden md:block'>
+                <p>Cool, Crisp, Classic</p>
+                <p className='subtitle'>
+                    Sip the Spirit <br/> of Summer
+                </p>
+            </div>
+            <div className='view-cocktails'>
+                <p className='subtitle '>
+                    Every sip is a celebration of summer's spirit. Premium tequila, fresh lime, and a hint of mint come together in a cocktail that’s as refreshing as it is timeless.   
+                </p>
+                <a href='#cocktails'>View Cocktails</a>
+
+            </div>
+        </div>
+    </div>
+    
+    </section>
+
+    <div className='video absolute inset-0'>
+        <video
+        ref={videoRef}
+        src='/videos/output.mp4'
+        muted
+        playsInline
+        preload='auto'
+        />
+
+    </div>
+    </>
+  )
+}
+
+export default Hero
